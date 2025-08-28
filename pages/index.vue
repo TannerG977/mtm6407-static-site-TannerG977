66b9121a56047cc2fdd7616a4fc11b57608f5af9
Tanner <gree0638@algonquinlive.com>
2025-08-28 10:07:13 -0400

    changes profile pic to webp

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="containerdiv">
       <h2 class="hero-heading">Welcome to my web portfolio.</h2>
       <div class="intro-section">
-        <img class="avatar" alt="avatar" src="/assets/profilepic3.jpg"/>
+        <img class="avatar" alt="avatar" src="/assets/profilepic3.webp"/>
         <div class="intro-text">
           <p>
             My name is Tanner Green, and I am a web developer, videographer, and graphic designer. This site showcases projects that reflect the skills I’ve developed throughout my professional and academic career. I coded this portfolio using nuxt.js, vue.js, html, css, javascript, and bootstrap. Feel free to explore using the buttons below.
